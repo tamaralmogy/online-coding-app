0f27f8293134729368bc09280b8e94f0277df403
Tamar Almogy <tamar.almogy@post.runi.ac.il>
2024-05-21 12:43:30 +0300

    Deploy server to Heroku

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "../client/.env") });
+require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
